24f0a22bcb198c4ee17b868b60521c0ce8b42810
Kamogelo Seisa <kamogelo.seisa@entelect.co.za>
2023-03-29 10:19:34 +0200

    Adding bootstrapped spring proj

---
+++ src/main/java/com/entelect/training/javaspringscheduler/JavaSpringSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.entelect.training.javaspringscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entelect/training/javaspringscheduler/JavaSpringSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entelect.training.javaspringscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
